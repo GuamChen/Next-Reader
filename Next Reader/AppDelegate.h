ecf87bc7aef0a43f78e39b23c894d8ed911928cd
GuamChen <2960853985@qq.com>
2026-04-15 23:46:49 +0800

    Initial Commit

---
+++ Next Reader/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Next Reader
+//
+//  Created by Gavin on 2026/4/15.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Next Reader/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Next Reader
+//
+//  Created by Gavin on 2026/4/15.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
